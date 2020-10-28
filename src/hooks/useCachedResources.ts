0d10ffaf80812ce0ae0a39c0b1c364127f190c48
Joshua Kaluba <joshkaluba@gmail.com>
2020-10-28 01:38:05 -0400

    Created hooks

---
+++ src/hooks/useCachedResources.ts
@@ -0,0 +1,33 @@
+import { Ionicons } from '@expo/vector-icons';
+import * as Font from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+import * as React from 'react';
+
+export default function useCachedResources() {
+  const [isLoadingComplete, setLoadingComplete] = React.useState(false);
+
+  React.useEffect(() => {
+    async function loadResourcesAndDataAsync() {
+      try {
+        SplashScreen.preventAutoHideAsync();
+
+        await Font.loadAsync({
+          ...Ionicons.font,
+          'space-mono': require('../../assets/fonts/SpaceMono-Regular.ttf'),
+          'CounterFont': require('../../assets/fonts/CounterFont.ttf'),
+          'Roboto_medium': require('../../assets/fonts/Roboto-Medium.ttf')
+        });
+      } catch (e) {
+        // TODO: connect to an error reporting service
+        console.warn(e);
+      } finally {
+        setLoadingComplete(true);
+        SplashScreen.hideAsync();
+      }
+    }
+
+    loadResourcesAndDataAsync();
+  }, []);
+
+  return isLoadingComplete;
+}
