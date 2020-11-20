4728c9d5cc38ca435e7988a7dbb9080026865fed
Joshua Kaluba <joshkaluba@gmail.com>
2020-11-20 17:31:40 -0500

    Added generateGuid and toast mechanism

--- src/utilities/Lib.ts
+++ src/utilities/Lib.ts
@@ -1,3 +1,5 @@
+//@ts-ignore
+import Toast from 'react-native-toast-message';
 
 const Lib = {
     showError(error: any) {
@@ -5,6 +7,22 @@ const Lib = {
         setTimeout(() => {
             alert(error);
         }, 500);
+    },
+
+    generateGuid(){
+        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+            var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+            return v.toString(16);
+          });
+    },
+
+    showSuccessMessageToast(title:string, message:string){
+        Toast.show({
+            position: 'bottom',
+            visibilityTime: 5000,
+            text1: title,
+            text2: message
+        });
     }
 };
 
