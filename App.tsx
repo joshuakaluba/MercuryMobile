ba5d5f8991a8e5be3087e7e7ca95cbc78ae71128
Joshua Kaluba <joshkaluba@gmail.com>
2020-11-20 17:17:04 -0500

    Formatting and added toast provider

--- App.tsx
+++ App.tsx
@@ -3,33 +3,37 @@ import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { StyleProvider } from 'native-base';
 import { Portal, Provider } from 'react-native-paper';
-import useCachedResources from './src/hooks/useCachedResources';
-import useColorScheme from './src/hooks/useColorScheme';
-import Navigation from './src/navigation';
-
+//@ts-ignore
+import Toast from 'react-native-toast-message';
 // @ts-ignore
 import getTheme from './native-base-theme/components';
 // @ts-ignore
 import commonColor from './native-base-theme/variables/commonColor';
+import useCachedResources from './src/hooks/useCachedResources';
+import useColorScheme from './src/hooks/useColorScheme';
+import Navigation from './src/navigation';
 
-export default function App() {
-  const isLoadingComplete = useCachedResources();
-  const colorScheme = useColorScheme();
+const App = () => {
+    const isLoadingComplete = useCachedResources();
+    const colorScheme = useColorScheme();
 
-  if (!isLoadingComplete) {
-    return null;
-  } else {
-    return (
-      <StyleProvider style={getTheme(commonColor)}>
-        <SafeAreaProvider>
-          <Provider>
-            <Portal>
-              <Navigation colorScheme={colorScheme} />
-              <StatusBar />
-            </Portal>
-          </Provider>
-        </SafeAreaProvider>
-      </StyleProvider>
-    );
-  }
+    if (!isLoadingComplete) {
+        return null;
+    } else {
+        return (
+            <StyleProvider style={getTheme(commonColor)}>
+                <SafeAreaProvider>
+                    <Provider>
+                        <Portal>
+                            <Navigation colorScheme={colorScheme} />
+                            <Toast ref={(ref: any) => Toast.setRef(ref)} />
+                            <StatusBar />
+                        </Portal>
+                    </Provider>
+                </SafeAreaProvider>
+            </StyleProvider>
+        );
+    }
 }
+
+export default App;
